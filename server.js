4046501129e3c6b384504898973fcc2a008d0907
BlackNeo <webscribo@gmail.com>
2017-11-18 00:57:10 +0100

    prod fix (recover 2)

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const http = require('http');
 const path = require('path');
 
 const api = require('./server/routes/api');
 
-const port = 3000;
+const portDev = 3000;
+const portProd = 8080;
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use('/api', api);
 
-app.get('*', (req, res) => {
+app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-app.listen(port, function() {
-    console.log("Server running on localhost :" + port);
-});
+const server = http.createServer(app);
+
+if (app.get('env') === 'development')
+{
+    server.listen(portDev, function() {
+        console.log("Server running on localhost :" + portDev);
+    });
+} else {
+    server.listen(portProd, function () {
+        console.log("Server running on localhost :" + portProd);
+        });
+}
