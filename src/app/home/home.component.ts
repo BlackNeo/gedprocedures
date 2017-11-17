e8dc0e40dc96444bba82a0999aa66f456df42410
BlackNeo <webscribo@gmail.com>
2017-11-17 05:53:15 +0100

    set urlPrefix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  urlPrefix = 'ged';
+  urlPrefix = 'gedprocedures';
 
   constructor() { }
 
