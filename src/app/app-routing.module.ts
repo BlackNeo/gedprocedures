bb4fba3dd674d48cfe11cc413a881eb956223aed
BlackNeo <webscribo@gmail.com>
2017-11-22 03:01:26 +0100

    Admin Panel Integration + JS files fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { PanelComponent } from './panel/panel.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', component: HomeComponent},
+  {path: 'panel', component: PanelComponent},
   {path: 'login', component: LoginComponent}
 ];
 
