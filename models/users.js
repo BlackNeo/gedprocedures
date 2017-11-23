2121c9d777683a4f94df2712e6e527ca556cc6f3
BlackNeo <webscribo@gmail.com>
2017-11-23 02:05:58 +0100

    Auto-generated commit

---
+++ models/users.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const usersSchema = new Schema ({
+    username: String,
+    password: String
+});
+
+module.exports = mongoose.model('users', usersSchema, 'users');
