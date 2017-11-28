6622e29bd2be37d5721be8cdb86711adab3ef8ad
BlackNeo <webscribo@gmail.com>
2017-11-29 00:38:15 +0100

    Ged Mean v1.03

--- src/assets/panel/js/sb-admin-charts.js
+++ src/assets/panel/js/sb-admin-charts.js
@@ -2,16 +2,16 @@
 // -- Set new default font family and font color to mimic Bootstrap's default styling
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
-var ctx = document.getElementById("myBarChart");
+var ctx = document.getElementById("bar-chart-docs");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin"],
     datasets: [{
-      label: "Revenue",
+      label: "Document",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [5, 13, 22, 15, 25, 24],
     }],
   },
   options: {
@@ -30,7 +30,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
+          max: 30,
           maxTicksLimit: 5
         },
         gridLines: {
