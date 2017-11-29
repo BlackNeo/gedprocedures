c629f764d23239a797e2190954dacc8e5e5ae202
BlackNeo <webscribo@gmail.com>
2017-11-29 13:49:44 +0100

    Ged Mean v1.1.02

--- src/app/ged/ged.component.ts
+++ src/app/ged/ged.component.ts
@@ -18,6 +18,7 @@ export class GedComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
+    $.getScript('../../assets/panel/vendor/jquery-easing/jquery.easing.min.js', function(){});
     $.getScript('../../assets/panel/vendor/chart.js/Chart.js', function(){});
     setTimeout(() => {
       $.getScript('../../assets/panel/js/sb-admin.js', function(){});
