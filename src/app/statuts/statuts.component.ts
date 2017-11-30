acaf729c5e2ccaa2491d541d9f8b895d72b10efe
BlackNeo <webscribo@gmail.com>
2017-12-01 00:38:31 +0100

    Ged Mean ready to tag 0.1.0 : Front-ok + fix routes

--- src/app/statuts/statuts.component.ts
+++ src/app/statuts/statuts.component.ts
@@ -17,11 +17,9 @@ export class StatutsComponent implements OnInit, AfterViewInit {
     $.getScript('../../assets/panel/vendor/jquery-easing/jquery.easing.min.js', function(){});
     $.getScript('../../assets/panel/vendor/datatables/jquery.dataTables.js', function(){});
     $.getScript('../../assets/panel/vendor/datatables/dataTables.bootstrap4.js', function(){});
-    $.getScript('../../assets/panel/vendor/chart.js/Chart.js', function(){});
     setTimeout(() => {
       $.getScript('../../assets/panel/js/sb-admin.js', function(){});
-      $.getScript('../../assets/panel/js/sb-admin-datatables.js', function(){});
-      $.getScript('../../assets/panel/js/sb-admin-charts.js', function(){});
+      $.getScript('../../assets/panel/js/statuts-datatables.js', function(){});
     }, 1000);
   }
 }
