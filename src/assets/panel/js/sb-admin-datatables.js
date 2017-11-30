844cbd77eeb58c6f71a557448d471461ef80ee09
BlackNeo <webscribo@gmail.com>
2017-11-30 02:55:33 +0100

    Ged Mean ready to tag 0.1.0 test 0.13

--- src/assets/panel/js/sb-admin-datatables.js
+++ src/assets/panel/js/sb-admin-datatables.js
@@ -1,6 +1,4 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#bills-table').DataTable();
-  $('#official-table').DataTable();
-  $('#administrative-table').DataTable();
+  $('#billsTable').DataTable();
 });
