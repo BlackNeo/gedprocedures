d7410efdba98a2e0f2a930d4206f5db5999d3377
BlackNeo <webscribo@gmail.com>
2017-11-30 02:42:44 +0100

    Ged Mean ready to tag 0.1.0 test 0.11

--- src/app/safe-deposit-box/safe-deposit-box.component.ts
+++ src/app/safe-deposit-box/safe-deposit-box.component.ts
@@ -14,11 +14,10 @@ export class SafeDepositBoxComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    $.getScript('../../assets/panel/vendor/jquery-easing/jquery.easing.min.js', function(){});
-    $.getScript('../../assets/panel/vendor/datatables/dataTables.js', function(){});
-    $.getScript('../../assets/panel/vendor/datatables/dataTables.bootstrap4.js', function(){});
     $.getScript('../../assets/panel/vendor/chart.js/Chart.js', function(){});
     setTimeout(() => {
+      $.getScript('../../assets/panel/vendor/datatables/dataTables.js', function(){});
+      $.getScript('../../assets/panel/vendor/datatables/dataTables.bootstrap4.js', function(){});
       $.getScript('../../assets/panel/js/sb-admin.js', function(){});
       $.getScript('../../assets/panel/js/sb-admin-datatables.js', function(){});
       $.getScript('../../assets/panel/js/sb-admin-charts.js', function(){});
