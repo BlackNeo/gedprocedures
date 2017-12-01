6336e27211d57e1f5ea02f22ac708bddf17d7031
BlackNeo <webscribo@gmail.com>
2017-12-01 02:13:42 +0100

    Ged Mean ready to tag 0.1.0 : last fix v0.11

--- src/assets/panel/js/dashboard-datatables.js
+++ src/assets/panel/js/dashboard-datatables.js
@@ -1,4 +1,7 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
     $('#bills-table').DataTable();
+    $('#official-table').DataTable();
+    $('#administrative-table').DataTable();
+    $('#status-table').DataTable();
   });
