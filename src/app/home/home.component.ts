04147e3817ecbd844b3b85650959637e6c60179d
BlackNeo <webscribo@gmail.com>
2018-05-17 08:44:48 +0200

    FormMail 0.3

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { SessionStorageService } from 'ngx-webstorage';
+declare var $: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   mailFormToJSON: string;
-  @Input() userMail: string;
+  @Input() userMail: any;
 
   constructor( private _sessionStorage: SessionStorageService ) { }
 
@@ -18,10 +19,16 @@ export class HomeComponent implements OnInit {
     this.userMail = '';
   }
 
+  ngAfterViewInit() {
+    $.getScript('../../../node_modules/jquery/dist/jquery.min.js', function(){});
+    $.getScript('../../assets/panel/vendor/jquery-easing/jquery.easing.min.js', function(){});
+    $.getScript('../../assets/panel/js/sb-admin.js', function(){});
+  }
+
   submitForm(formMail: NgForm) {
     this.mailFormToJSON = JSON.stringify(formMail.value);
     this.userMail = JSON.parse(this.mailFormToJSON);
-    this._sessionStorage.store('isLogin', this.userMail);
+    this._sessionStorage.store('mail', this.userMail);
     console.log(this.mailFormToJSON);
   }
 
