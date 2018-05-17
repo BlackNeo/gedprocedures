47a262fee93920043428603442a83534188bdb01
BlackNeo <webscribo@gmail.com>
2018-05-17 08:55:15 +0200

    FormMail 0.31 Test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Input, Output, AfterViewInit, EventEmitter } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { SessionStorageService } from 'ngx-webstorage';
-declare var $: any;
+// declare var $: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit, AfterViewInit {
+export class HomeComponent implements OnInit {
 
   mailFormToJSON: string;
   @Input() userMail: any;
+  // @Output() mailToRegister = new EventEmitter<any>();
 
   constructor( private _sessionStorage: SessionStorageService ) { }
 
@@ -19,17 +20,19 @@ export class HomeComponent implements OnInit, AfterViewInit {
     this.userMail = '';
   }
 
-  ngAfterViewInit() {
-    $.getScript('../../../node_modules/jquery/dist/jquery.min.js', function(){});
-    $.getScript('../../assets/panel/vendor/jquery-easing/jquery.easing.min.js', function(){});
-    $.getScript('../../assets/panel/js/sb-admin.js', function(){});
-  }
+  // ngAfterViewInit() {
+  //   $.getScript('../../../node_modules/jquery/dist/jquery.min.js', function(){});
+  //   $.getScript('../../assets/panel/vendor/jquery-easing/jquery.easing.min.js', function(){});
+  //   $.getScript('../../assets/panel/js/sb-admin.js', function(){});
+  // }
 
   submitForm(formMail: NgForm) {
-    this.mailFormToJSON = JSON.stringify(formMail.value);
-    this.userMail = JSON.parse(this.mailFormToJSON);
-    this._sessionStorage.store('mail', this.userMail);
-    console.log(this.mailFormToJSON);
+    // this.mailFormToJSON = JSON.stringify(formMail.value);
+    // this.userMail = JSON.parse(this.mailFormToJSON);
+    // // this._sessionStorage.store('mail', this.userMail);
+    // this.mailToRegister.emit(this.userMail);
+    alert(formMail.value);
+    console.log(formMail.value);
   }
 
 }
